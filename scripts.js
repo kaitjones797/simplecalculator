0e8fe0c1e2b71dacba1797056ada3dcb5e04b3b7
Kaitlyn Jones <kaitlynjones797@gmail.com>
2023-12-13 22:15:34 -0500

    added functions to display add,sub,mult,div

---
+++ scripts.js
@@ -0,0 +1,16 @@
+function addNumbers(num1, num2) {
+  
+    return num1 + num2;
+}
+
+function subtractNumbers(num1,num2) {
+    return num1-num2
+}
+
+function multiplyNumbers(num1,num2) {
+    return num1 * num2
+}
+
+function divdeNumbers(num1,num2) {
+    return num1/num2
+}
